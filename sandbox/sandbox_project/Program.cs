7427f2f2e221009b4eca318988783a7e6df2cda5
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-10-27 07:09:13 -0600

    Played in the sandbox

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 public class Program
 {
@@ -7,6 +9,36 @@ public class Program
         // This project is here for you to use as a "Sandbox" to play around
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
+
+        string usersInput = "aabccdeff";
+        var charCount = Iterator(usersInput);
+
+        // Find the first character in the original string with a count of 1
+        char? firstNonRepeated = null;
+        foreach (var ch in usersInput)
+        {
+            if (charCount.TryGetValue(ch, out var c) && c == 1)
+            {
+                firstNonRepeated = ch;
+                break;
+            }
+        }
+
+        if (firstNonRepeated.HasValue)
+            Console.WriteLine($"{firstNonRepeated}");
+        else
+            Console.WriteLine("null");
+
+        static Dictionary<char, int> Iterator(string userInput)
+        {
+            var counts = new Dictionary<char, int>();
+            foreach (var character in userInput)
+            {
+                if (counts.ContainsKey(character)) counts[character]++;
+                else counts[character] = 1;
+            }
+            return counts;
+        }
 
         Console.WriteLine("Hello Sandbox World!");
     }
